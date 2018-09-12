b65d54eb7b317169ad8d970627aad0481d5534a7
duykhanh992 <duykhanh992@gmail.com>
2018-09-12 11:12:20 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 
 $(function(){
 var choice = [element_11,element_12,element_14,element_18]
-  $("#element_6_1").click(function(){
+  $("#element_6_1").checked(function(){
     
     alert("checked");
   }
